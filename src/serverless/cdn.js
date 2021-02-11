5188fe62a986311bab8c7223b55a5a8ba9a83f4e
Tianya Liu <tianyaliu1995@gmail.com>
2021-02-11 00:59:42 -0500

    update caching

--- src/serverless/cdn.js
+++ src/serverless/cdn.js
@@ -1,4 +1,4 @@
-const DEFAULT_S_MAX_AGE = 5 * 60
+const DEFAULT_S_MAX_AGE = 24 * 60 * 60
 const DEFAULT_MAX_STALE = Math.max(24 * 60 * 60 - DEFAULT_S_MAX_AGE, 1)
 
 export function setCacheStaleWhileRevalidate (res, sMaxAge = DEFAULT_S_MAX_AGE, maxStale = DEFAULT_MAX_STALE) {
