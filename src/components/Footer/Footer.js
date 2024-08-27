21fa842b2a14e82b529329e19a2a6bd67f7f24d5
tianyaliu95 <tianyaliu1995@gmail.com>
2024-08-26 23:55:48 -0700

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer () {
   return (
     <footer className='flex flex-col w-full py-8 lg:py-6 justify-center items-center text-xs lg:text-sm font-semibold text-grays-600 bg-grays-200'>
-      <p className='my-1'>Copyright © 2016 - 2022 江苏行者房车有限公司</p>
+      <p className='my-1'>Copyright © 2016 - 2024 江苏行者房车有限公司</p>
       <p className='my-1'>
         <a className='mr-2 font-bold text-blue-700 underline' href='https://www.tianyaliu.ca' target='_blank' rel='noopener noreferrer'>Tianya Liu</a><i>All Rights Reserved</i>
       </p>
