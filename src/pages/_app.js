65248505a5ae2e3ab1a5f9e74bf3e5a961ad93af
tianyaliu95 <tianyaliu1995@gmail.com>
2024-10-11 23:05:36 -0700

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,15 @@
+import { Analytics } from "@vercel/analytics/react"
+
 import '@/styles/globals.css'
 import './_app.css'
 
-function MyApp ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Analytics/>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
